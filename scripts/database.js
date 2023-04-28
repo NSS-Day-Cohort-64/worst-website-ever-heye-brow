8e8a08d15ac75e38b8d9434cef179033e947aeb4
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-04-28 14:21:57 -0400

    added database and brow photos

---
+++ scripts/database.js
@@ -0,0 +1,25 @@
+
+const database = {
+    locations: [
+        {
+            id: 1,
+            name: "East Nashville",
+            address: "519 Gallatin Avenue, Nashville, TN 37206",
+            phone: "615.915.1354",
+            monday: "8AM-6PM",
+            week: "8AM-8PM",
+            saturday: "9AM-6PM",
+            sunday: "10AM-5PM"
+        },
+        {
+            id: 2,
+            name: "8th Avenue South",
+            address: "2106 8th Avenue South, Nashville, TN 37204",        
+            phone: "615.499.5879",
+            monday: "8AM-8PM",
+            week: "8AM-8PM",
+            saturday: "9AM-6PM",
+            sunday: "10AM-5PM"
+        },
+    ]
+}
